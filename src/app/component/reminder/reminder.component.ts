54ef323f119c01ff58c6c5aea4c021c73d4e09c7
mounikanarla <nmounika358@gmail.com>
2018-11-14 14:42:06 +0530

    commited

--- src/app/component/reminder/reminder.component.ts
+++ src/app/component/reminder/reminder.component.ts
@@ -155,38 +155,116 @@ reminder(){
   // this.myDate.date=this.value;
   
 }
-customDate(){
-//  var date=new Date(this.value.date.getFullYear(),this.value.date.getMonth(),this.value.date.getDate())
-// console.log(date);
-console.log(this.myDate.date)
- //  this.eventEmitReminder.emit(date);
-var timearray=this.myDate.split(' ');
-var timearray2=timearray[0].split(':')
-timearray2.push(timearray[1])
-var min=Number(timearray2[1]);
-var hr=Number(timearray2[0]);
-if(timearray2[2]=="PM" && hr<12){
-  hr=hr+12;
-}
- if(this.noteid!=null)
- {
- this.body =
-   {
-     'noteIdList': [this.noteid.id],
-     'reminder': new Date(this.myDate.date.getFullYear(),this.myDate.date.getMonth(),this.myDate.date.getDate()+0,hr,min,0)
-   }
- this.httpService.delPost('/notes/addUpdateReminderNotes', this.body,localStorage.getItem('id'))
-   .subscribe(data => {
-     console.log("success in week reminders",data);
-     this.eventEmit.emit({});
-     // this.eventEmitReminder.emit(date);
-  },
-     error => {
-       console.log("error in week reminders",error)
-  })
-}
+// customDate(){
+// //  var date=new Date(this.value.date.getFullYear(),this.value.date.getMonth(),this.value.date.getDate())
+// // console.log(date);
+// console.log(this.myDate.date)
+//  //  this.eventEmitReminder.emit(date);
+// var timearray=this.myDate.split(' ');
+// var timearray2=timearray[0].split(':')
+// timearray2.push(timearray[1])
+// var min=Number(timearray2[1]);
+// var hr=Number(timearray2[0]);
+// if(timearray2[2]=="PM" && hr<12){
+//   hr=hr+12;
+// }
+//  if(this.noteid!=null)
+//  {
+//  this.body =
+//    {
+//      'noteIdList': [this.noteid.id],
+//      'reminder': new Date(this.myDate.date.getFullYear(),this.myDate.date.getMonth(),this.myDate.date.getDate()+0,hr,min,0)
+//    }
+//  this.httpService.delPost('/notes/addUpdateReminderNotes', this.body,localStorage.getItem('id'))
+//    .subscribe(data => {
+//      console.log("success in week reminders",data);
+//      this.eventEmit.emit({});
+//      // this.eventEmitReminder.emit(date);
+//   },
+//      error => {
+//        console.log("error in week reminders",error)
+//   })
+// }
 
 
-}
+// }
+// }
+
+reminderBody={
+  "date": new FormControl(new Date()),
+  "time":""
 }
 
+addRemCustom(date,timing){
+  timing.match('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$');
+  if(timing=='8:00 AM'){
+    this.body = {
+      "noteIdList": [this.noteid.id],
+      "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 8, 0, 0, 0)
+    }
+    // this.httpService.postdeletecard('notes/addUpdateReminderNotes',  this.body,this.token).subscribe((result) => {
+      
+    //   this.reminderevent.emit()
+    // })
+
+
+
+}
+else if(timing=='1:00 PM'){
+
+  this.body = {
+    "noteIdList": [this.noteid.id],
+    "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 13, 0, 0, 0)
+  }
+  // this.httpService.postdeletecard('notes/addUpdateReminderNotes',this.body,this.token).subscribe((result) => {
+    
+  //   this.reminderevent.emit()
+  // })
+}
+else if(timing=='6:00 PM'){
+  this.body = {
+    "noteIdList": [this.noteid.id],
+    "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 18, 0, 0, 0)
+  }
+  // this.httpService.postdeletecard('notes/addUpdateReminderNotes',this.body,this.token).subscribe((result) => {
+   
+  //   this.reminderevent.emit()
+  // })
+}
+else if(timing=='9:00 PM'){
+  this.body = {
+    "noteIdList": [this.noteid.id],
+    "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 21, 0, 0, 0)
+  }
+  
+}
+else if(timing==this.reminderBody.time){
+  var splitTime=this.reminderBody.time.split("",8);
+  var hour= Number(splitTime[0]+splitTime[1]);
+  var minute= Number(splitTime[3]+splitTime[4]);
+  var ampm = (splitTime[6]+splitTime[7]);
+ 
+  if(ampm=='AM' || ampm=='am'){
+    this.body = {
+      "noteIdList": [this.noteid.id],
+      "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), hour, minute, 0, 0)
+    }
+    this.httpService.delPost('notes/addUpdateReminderNotes', this.body,localStorage.getItem('id')).subscribe((result) => {
+      
+      this.eventEmitReminder.emit()
+    })
+  }else if(ampm=='PM' || ampm=='pm'){
+    this.body = {
+      "noteIdList": [this.noteid.id],
+      "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), hour+12, minute, 0, 0)
+    }
+    
+  }
+  
+}
+this.httpService.delPost('notes/addUpdateReminderNotes',this.body,localStorage.getItem('id')).subscribe((result) => {
+    
+  this.eventEmitReminder.emit()
+})
+}
+}

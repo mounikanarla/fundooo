7bb03e5977afe84de062f18a83425ce7f5ef0327
mounikanarla <nmounika358@gmail.com>
2018-11-14 14:49:08 +0530

    commit

--- src/app/component/reminder/reminder.component.ts
+++ src/app/component/reminder/reminder.component.ts
@@ -31,12 +31,12 @@ export const MY_FORMATS = {
 })
 export class ReminderComponent implements OnInit {
   selectedValue: string;
-  dates: any = [
-    {value: 'Morning-0', viewPeriod: 'Morning', viewTime: '8:00 AM'},
-    {value: 'Afternoon-1', viewPeriod: 'Afternoon' ,viewTime:'1:00 PM'},
-    {value: 'Evening-2', viewPeriod: 'Evening',viewTime:'6:00 PM'},
-    {value: 'Night-3', viewPeriod: 'Night',    viewTime:'8:00 PM'},
-    {value: 'Custom-4', viewPeriod: 'Custom'},
+  remind: any = [
+    {value: 'Morning', viewPeriod: 'Morning', viewTime: '08:00 AM'},
+    {value: 'Afternoon', viewPeriod: 'Afternoon' ,viewTime:'01:00 PM'},
+    {value: 'Evening', viewPeriod: 'Evening',viewTime:'06:00 PM'},
+    {value: 'Night', viewPeriod: 'Night',    viewTime:'08:00 PM'},
+    {value: 'Custom', viewPeriod: 'Custom'},
 
   ];
 
@@ -155,40 +155,7 @@ reminder(){
   // this.myDate.date=this.value;
   
 }
-// customDate(){
-// //  var date=new Date(this.value.date.getFullYear(),this.value.date.getMonth(),this.value.date.getDate())
-// // console.log(date);
-// console.log(this.myDate.date)
-//  //  this.eventEmitReminder.emit(date);
-// var timearray=this.myDate.split(' ');
-// var timearray2=timearray[0].split(':')
-// timearray2.push(timearray[1])
-// var min=Number(timearray2[1]);
-// var hr=Number(timearray2[0]);
-// if(timearray2[2]=="PM" && hr<12){
-//   hr=hr+12;
-// }
-//  if(this.noteid!=null)
-//  {
-//  this.body =
-//    {
-//      'noteIdList': [this.noteid.id],
-//      'reminder': new Date(this.myDate.date.getFullYear(),this.myDate.date.getMonth(),this.myDate.date.getDate()+0,hr,min,0)
-//    }
-//  this.httpService.delPost('/notes/addUpdateReminderNotes', this.body,localStorage.getItem('id'))
-//    .subscribe(data => {
-//      console.log("success in week reminders",data);
-//      this.eventEmit.emit({});
-//      // this.eventEmitReminder.emit(date);
-//   },
-//      error => {
-//        console.log("error in week reminders",error)
-//   })
-// }
 
-
-// }
-// }
 
 reminderBody={
   "date": new FormControl(new Date()),
@@ -202,13 +169,6 @@ addRemCustom(date,timing){
       "noteIdList": [this.noteid.id],
       "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 8, 0, 0, 0)
     }
-    // this.httpService.postdeletecard('notes/addUpdateReminderNotes',  this.body,this.token).subscribe((result) => {
-      
-    //   this.reminderevent.emit()
-    // })
-
-
-
 }
 else if(timing=='1:00 PM'){
 
@@ -216,20 +176,14 @@ else if(timing=='1:00 PM'){
     "noteIdList": [this.noteid.id],
     "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 13, 0, 0, 0)
   }
-  // this.httpService.postdeletecard('notes/addUpdateReminderNotes',this.body,this.token).subscribe((result) => {
-    
-  //   this.reminderevent.emit()
-  // })
+  
 }
 else if(timing=='6:00 PM'){
   this.body = {
     "noteIdList": [this.noteid.id],
     "reminder": new Date(date.getFullYear(), date.getMonth(), date.getDate(), 18, 0, 0, 0)
   }
-  // this.httpService.postdeletecard('notes/addUpdateReminderNotes',this.body,this.token).subscribe((result) => {
-   
-  //   this.reminderevent.emit()
-  // })
+
 }
 else if(timing=='9:00 PM'){
   this.body = {
